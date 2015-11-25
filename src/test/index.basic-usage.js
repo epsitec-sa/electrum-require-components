c7c9659d57583c40b2095df69d1d15bf571237f3
Pierre Arnaud <arnaud@epsitec.ch>
2015-11-25 15:17:25 +0100

    First empty project with test configured.

---
+++ src/test/index.basic-usage.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import {expect} from 'mai-chai';
+
+describe ('Require components', () => {
+  describe ('list()', () => {
+    it ('produces the expected list of components', () => {
+      expect (true).to.be.true ();
+    });
+  });
+});
---
+++ wallaby.conf.js
@@ -0,0 +1,34 @@
+/*globals __dirname */
+'use strict';
+
+var babel = require ('babel-core');
+var fs = require ('fs');
+var path = require ('path');
+
+var babelConfig = JSON.parse (fs.readFileSync (path.join (__dirname, '.babelrc')));
+
+babelConfig.babel = babel;
+
+module.exports = function (wallaby) {
+  return {
+    files: [
+      {pattern: 'test/test-helper.js'},
+      {pattern: 'src/*.js'}
+    ],
+    tests: [
+      {pattern: 'src/test/**/*.js'},
+    ],
+    compilers: {
+      '**/*.js*': wallaby.compilers.babel (babelConfig)
+    },
+    debug: true,
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+    bootstrap: function () {
+    },
+    teardown: function () {
+    }
+  };
+};
